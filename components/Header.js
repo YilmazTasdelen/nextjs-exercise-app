f28763d0984f964b8403bfb48426fae08335c36c
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-17 12:49:58 +0300

    .

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,37 @@
 import { DingtalkOutlined, DownloadOutlined } from '@ant-design/icons';
 import { Col, Row, Button, Divider } from 'antd';
 import Link from 'next/link';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../utils/Store';
 
 function Header() {
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
+
+  const [userName, setUserName] = useState();
+
+  useEffect(() => {
+    userInfo ? setUserName(userInfo.name) : setUserName(undefined);
+  }, []);
+
+  const userButtonHandler = () => {
+    return userName ? (
+      <div>{userName}</div>
+    ) : (
+      <Button
+        style={{
+          marginTop: -5,
+          fontSize: 15,
+          fontFamily: 'fantasy',
+          fontWeight: 'lighter',
+        }}
+        shape="round"
+        danger
+      >
+        Sign up/Login
+      </Button>
+    );
+  };
 
   return (
     <div className="header">
@@ -62,24 +87,7 @@ function Header() {
             </Col>
           </Row>
         </Col>
-        <Col span={4}>
-          {userInfo ? (
-            userInfo.name
-          ) : (
-            <Button
-              style={{
-                marginTop: -5,
-                fontSize: 15,
-                fontFamily: 'fantasy',
-                fontWeight: 'lighter',
-              }}
-              shape="round"
-              danger
-            >
-              Sign up/Login
-            </Button>
-          )}
-        </Col>
+        <Col span={4}>{userButtonHandler()}</Col>
       </Row>
       <Divider style={{ marginTop: 12 }} />
     </div>
--- pages/Exercises.js
+++ pages/Exercises.js
@@ -1,7 +1,171 @@
-import React from 'react';
+import { RollbackOutlined } from '@ant-design/icons';
+import { Button, Col, Row } from 'antd';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import Model, { IExerciseData, IMuscleStats } from 'react-body-highlighter';
 
 const Exercises = () => {
-  return <div>Exercises</div>;
+  const handleRouteStart = () => {};
+  const [flip, setFlip] = useState('flip-card-inner');
+
+  const data = [
+    { muscles: ['chest', 'triceps', 'front-deltoids', 'quadriceps'] },
+    { muscles: ['chest'] },
+  ];
+
+  const data2 = [
+    {
+      name: 'test',
+      muscles: [
+        'trapezius',
+        'upper-back',
+        // 'lower-back',
+        'calves',
+        // 'adductor',
+        'hamstring',
+        'gluteal',
+      ],
+    },
+    { name: 'test', muscles: ['trapezius'] },
+  ];
+
+  const data3 = [
+    { name: 'test', muscles: ['trapezius', 'upper-back', 'lower-back'] },
+    { name: 'test', muscles: ['trapezius'] },
+  ];
+
+  // ('adductor');
+  // ('hamstring');
+  // ('quadriceps');
+  // ('abductors');
+  // ('calves');
+  // ('gluteal');
+
+  const trunBodyImage = () => {
+    console.log('rotated');
+    if (flip === 'flip-card-inner') {
+      setFlip('flip-card-inner-rotated');
+    } else {
+      setFlip('flip-card-inner');
+    }
+  };
+  const handleClick = React.useCallback(
+    ({ muscle, data }) => {
+      const { exercises, frequency } = data;
+
+      alert(
+        `You clicked the ${muscle}! You've worked out this muscle ${frequency} times through the following exercises: ${JSON.stringify(
+          exercises
+        )}`
+      );
+    },
+    [data]
+  );
+  return (
+    <Row>
+      <Col span={3}></Col>
+      <Col span={4} style={{ backgroundColor: 'white' }}>
+        <div className="flip-card">
+          <Button type="dashed" shape="round" style={{ width: '100%' }}>
+            <RollbackOutlined
+              onClick={() => trunBodyImage()}
+              //   style={{ marginLeft: '15%' }}
+            />{' '}
+            Turn The body
+          </Button>
+          <div className={flip}>
+            <div className="flip-card-front">
+              {' '}
+              <Model
+                data={data}
+                highlightedColors={[
+                  '#ccbf93',
+                  '#ab96b0',
+                  '#e65a5a',
+                  '#db2f2f',
+                  '#95cc93',
+                ]}
+                style={{ padding: 15 }}
+                onClick={handleClick}
+              />
+            </div>
+            <div className="flip-card-back">
+              {' '}
+              <Model
+                type="posterior"
+                data={data2}
+                highlightedColors={[
+                  '#ccbf93',
+                  '#ab96b0',
+                  '#e65a5a',
+                  '#db2f2f',
+                  '#95cc93',
+                ]}
+                style={{ padding: 15 }}
+                // onClick={handleClick}
+              />
+            </div>
+          </div>
+        </div>
+      </Col>
+      <Col span={8}>
+        <div
+          style={{
+            textAlign: 'center',
+            fontSize: 20,
+            fontFamily: 'fantasy',
+            // color: '#94989c',
+          }}
+        >
+          BUILD YOUR PERFECT BODY
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            fontSize: 15,
+            fontWeight: 'lighter',
+            fontFamily: 'fantasy',
+            color: '#94989c',
+          }}
+        >
+          Get a personalized workout program and track your pocess...
+        </div>
+
+        {/* <Row>
+          <Col span={12}>
+            <Model
+              data={data}
+              highlightedColors={[
+                '#ccbf93',
+                '#ab96b0',
+                '#e65a5a',
+                '#db2f2f',
+                '#95cc93',
+              ]}
+              style={{ width: '20rem', padding: '5rem' }}
+              onClick={handleClick}
+            />
+          </Col>
+          <Col span={12}>
+            <Model
+              type="posterior"
+              data={data2}
+              highlightedColors={[
+                '#ccbf93',
+                '#ab96b0',
+                '#e65a5a',
+                '#db2f2f',
+                '#95cc93',
+              ]}
+              style={{ width: '20rem', padding: '5rem' }}
+              // onClick={handleClick}
+            />
+          </Col>
+        </Row> */}
+        {/* <Divider /> */}
+      </Col>
+    </Row>
+  );
 };
 
 export default Exercises;
